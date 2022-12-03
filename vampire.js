0a60064f8e42a672368160f3d2e3ea32ec557b95
Krista Kourtessis <krista.kourtessis@telus.com>
2022-12-02 16:40:56 -0800

    pushing first commit

--- vampire.js
+++ vampire.js
@@ -10,21 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampires = this;
 
+    while (currentVampires.creator){
+      currentVampires = currentVampires.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+
+   return this.numberOfVampiresFromOriginal <  vampire.numberOfVampiresFromOriginal
 
   }
 
@@ -39,6 +49,34 @@ class Vampire {
 
   }
 }
+
+
+///--------Original's Offspring
+const original = new Vampire("Original", 1000);
+// console.log(original);
+
+const ansel = new Vampire("Ansel", 1100);
+const bart = new Vampire("Bart", 1100);
+
+original.addOffspring(ansel);
+original.addOffspring(bart);
+
+//----------Ansel's offspring
+const elgort = new Vampire("Elgort", 1200);
+const sarah = new Vampire("Sarah", 1200)
+
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah)
+//---------Elgorts Offspring
+
+const andrew = new Vampire("Andrew", 1300);
+
+elgort.addOffspring(andrew);
+
+//--------Sarah Zero offspring
+
+console.log(elgort)
+
 
 module.exports = Vampire;
 
